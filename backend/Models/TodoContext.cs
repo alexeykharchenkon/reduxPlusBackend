fb45a02dada87e7fd989a983fdaba598d757dab7
alexeykharchenkon <alexeykharchenkon@gmail.com>
2021-08-12 18:22:59 +0300

    first commit

---
+++ backend/Models/TodoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReduxWithBackend.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData( new TodoItem [] {
+                new TodoItem { Id = 1, Title = "Buy Bread", IsComplete = false },
+                new TodoItem { Id = 2, Title = "Buy Milk", IsComplete = false },
+                new TodoItem { Id = 3, Title = "Buy Beer", IsComplete = false },
+                }
+            );
+        }
+    }
+}
